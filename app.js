8e954448e3c5f3526daf29106cd747647df4de77
Damien Corticchiato <skaradams69@gmail.com>
2020-03-17 16:16:28 +0100

    call twitch token each time

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const app = express()
 
 
-app.get('/', async function (req, res) {
+app.use(async (req, res, next) => {
   const response = await axios.post(
     'https://id.twitch.tv/oauth2/token?',
     {},
@@ -18,18 +18,56 @@ app.get('/', async function (req, res) {
     console.log(error);
   })
   const token = response.data.access_token
-  
-  const game = await axios.get(
-    'https://api.twitch.tv/helix/streams?game_id=33214',
+  next();
+})
+
+app.get('/', async function(req, res) {
+  console.log("home");
+  const response = await axios.post(
+    'https://id.twitch.tv/oauth2/token?',
+    {},
     {
-      headers: {
-        Authorization: `Bearer ${ token }`
+      params: {
+        client_id: '47t4devxqry4861taxoikjrl7mvpl2',
+        client_secret: 'v9pq35ee8oqw4os4hxwyf0ivqe3b55',
+        grant_type: 'client_credentials'
       }
     }
   ).catch(error => {
     console.log(error);
   })
-  console.log(game);
+  const token = response.data.access_token
+
+  const users = await axios.get(
+    'https://api.twitch.tv/helix/users',
+    {
+      headers: {
+        Authorization: `Bearer ${ token }`,
+      },
+      params: {
+        login: 'skaradams'
+      }
+    }
+  ).catch(error => {
+    console.log(error);
+  })
+  const broadcaster_id = users.data.data[0].id;
+
+  const clips = await axios.get(
+    'https://api.twitch.tv/helix/clips',
+    {
+      headers: {
+        Authorization: `Bearer ${ token }`,
+      },
+      params: {
+        broadcaster_id
+      }
+    }
+  ).catch(error => {
+    console.log(error);
+  })
+
+  console.log(clips.data.data[0]);
   res.send('Hello World!')
 })
 
